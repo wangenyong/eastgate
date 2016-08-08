c85734c1089cdb6c5b30a97ed25cd796ef013b86
wangenyong <wangenyong@gmail.com>
2016-08-08 15:16:35 +0800

    Create DrawerLayout

---
+++ js/components/DrawerLayout.js
@@ -0,0 +1,36 @@
+/**
+ * @flow
+ */
+
+'use strict'
+
+import React, { Component, PropTypes } from 'react';
+import {
+  DrawerLayoutAndroid,
+} from 'react-native';
+
+const DrawerLayout = React.createClass({
+  _drawer: DrawerLayoutAndroid,
+
+  render: function() {
+    const {drawerPosition, ...props} = this.props;
+    const {Right, Left} = DrawerLayoutAndroid.positions;
+    return (
+      <DrawerLayoutAndroid
+        ref={(drawer) => this._drawer = drawer}
+        {...props}
+        drawerPosition={drawerPosition === 'right' ? Right : Left}
+      />
+    )
+  },
+
+  closeDrawer: function() {
+    this._drawer && this._drawer.closeDrawer();
+  },
+
+  openDrawer: function() {
+    this._drawer && this._drawer.openDrawer();
+  }  
+})
+
+export default DrawerLayout
