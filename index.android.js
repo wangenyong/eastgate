e7b4a03aeacbfe075a9a0121f2f8e5e3e79a01d1
wangenyong <wangenyong@gmail.com>
2016-08-16 10:32:49 +0800

    修改android back事件处理

--- index.android.js
+++ index.android.js
@@ -16,43 +16,9 @@ import {
   StyleSheet,
   Text,
   View,
-  BackAndroid
 } from 'react-native';
 
 const eastgate = React.createClass({
-   _handlers: ([]: Array<() => boolean>),
-
-  componentDidMount: function() {
-    BackAndroid.addEventListener('hardwareBackPress', this.handleBackButton);
-  },
-
-  componentWillUnmount: function() {
-    BackAndroid.removeEventListener('hardwareBackPress', this.handleBackButton);
-  },
-
-  getChildContext() {
-    return {
-      addBackButtonListener: this.addBackButtonListener,
-      removeBackButtonListener: this.removeBackButtonListener,
-    };
-  },
-
-  addBackButtonListener: function(listener: any) {
-    this._handlers.push(listener);
-  },
-
-  removeBackButtonListener: function(listener: any) {
-    this._handlers = this._handlers.filter((handler) => handler !== listener);
-  },
-
-  handleBackButton: function() {
-    for (let i = this._handlers.length - 1; i >= 0; i--) {
-      if (this._handlers[i]()) {
-        return true;
-      }
-    }
-    return false;
-  },
 
   render: function() {
     return (
@@ -62,10 +28,5 @@ const eastgate = React.createClass({
     )
   }
 })
-
-eastgate.childContextTypes = {
-  addBackButtonListener: React.PropTypes.func,
-  removeBackButtonListener: React.PropTypes.func,
-};
 
 AppRegistry.registerComponent('eastgate', () => eastgate);
--- js/modules/navigation/NavigationView.js
+++ js/modules/navigation/NavigationView.js
@@ -9,13 +9,54 @@ import {
   View,
   StyleSheet,
   Navigator,
-  Platform
+  Platform,
+  BackAndroid
 } from 'react-native';
 import TabViewContainer from './TabViewContainer';
 import MovieDetailView from '../list/MovieDetailView';
 import SettingView from '../setting/SettingView';
 
 const NavigationView = React.createClass({
+  _handlers: ([]: Array<() => boolean>),
+
+  componentDidMount: function() {
+    BackAndroid.addEventListener('hardwareBackPress', this.handleBackButton);
+  },
+
+  componentWillUnmount: function() {
+    BackAndroid.removeEventListener('hardwareBackPress', this.handleBackButton);
+  },
+
+  getChildContext() {
+    return {
+      addBackButtonListener: this.addBackButtonListener,
+      removeBackButtonListener: this.removeBackButtonListener,
+    };
+  },
+
+  addBackButtonListener: function(listener: any) {
+    this._handlers.push(listener);
+  },
+
+  removeBackButtonListener: function(listener: any) {
+    this._handlers = this._handlers.filter((handler) => handler !== listener);
+  },
+
+  handleBackButton: function() {
+    for (let i = this._handlers.length - 1; i >= 0; i--) {
+      if (this._handlers[i]()) {
+        return true;
+      }
+    }
+
+    const {navigator} = this.refs;
+    if (navigator && navigator.getCurrentRoutes().length > 1) {
+      navigator.pop();
+      return true;
+    }
+    
+    return false;
+  },
 
   render: function() {
     return (
@@ -47,6 +88,11 @@ const NavigationView = React.createClass({
     return <TabViewContainer navigator={navigator} />
   }
 });
+
+NavigationView.childContextTypes = {
+  addBackButtonListener: React.PropTypes.func,
+  removeBackButtonListener: React.PropTypes.func,
+};
 
 const styles = StyleSheet.create({
   container: {
